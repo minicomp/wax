f0ae336d8e26ce79152d042c27f48199db61d8f5
Marii <mn2802@columbia.edu>
2018-05-02 13:03:36 -0400

    update jekyll, redo logo, simplify search tests

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-gem 'jekyll', '~> 3.8'
+gem 'jekyll', '~> 3.8.1'
 
 group :development, :test do
   gem 'rspec'
--- spec/quoll_spec.rb
+++ spec/quoll_spec.rb
@@ -3,30 +3,23 @@ baseurl = config['baseurl'].to_s
 search_tests = config['quoll']
 perma_ext = config['permalink'] == 'pretty' ? "/" : ".html"
 
+
 search_tests.each do |search|
-  search_page = search[1]['page']
+  page = (baseurl + "/" + search[1]['page'] + perma_ext).gsub(/(\/+)/, '/')
   terms = search[1]['terms']
 
-  describe search_page, :type => :feature, :js => true do
-    before(:all) do
-      visit(baseurl + "/" + search_page + perma_ext)
-      @search_bar = find(:css, "#search")
-    end
+  describe page, :type => :feature, :js => true do
     it "has a search bar." do
+      visit(page)
+      @search_bar = find(:css, "#search")
       expect(@search_bar)
     end
     terms.each do |term|
       context "when searching the term \"" + term + "\"" do
-        before(:all) do
-          @search_bar.set term
-          @result_link = first(".result").first("a")['href']
-        end
-        after(:all) do
-          visit(baseurl + "/" + search_page + perma_ext)
-        end
-        it "yields at least 1 result" do
-          expect(@result_link)
-        end
+        after(:example) { visit(page) }
+        let(:search_bar) { @search_bar.set term }
+        let(:result_link) { @result_link = first(".result").first("a")['href'] }
+        it "yields at least 1 result" do expect(@result_link) end
         it "which sucessfully links to an existing page" do
           visit(@result_link)
           expect(have_text(term))
