2049ccee21ee5477a5eba250375ba7a810ef0bb7
Alex Gil <colibri.alex@gmail.com>
2020-12-03 12:41:05 -0500

    fix broken thumbnail link on search results

--- assets/search-ui.js
+++ assets/search-ui.js
@@ -31,7 +31,7 @@ function displayResult(item, fields, url) {
       meta.push(`<b>${fieldLabel}:</b> ${excerptedString(item[fieldLabel])}`);
     }
   }
-  return `<div class="result"><a href="${url}${link}">${thumb}<p><span class="title">${item.label}</span><br><span class="meta">${meta.join(' | ')}</span></p></a></div>`;
+  return `<div class="result"><a href="${url}/${link}">${thumb}<p><span class="title">${item.label}</span><br><span class="meta">${meta.join(' | ')}</span></p></a></div>`;
 }
 
 function startSearchUI(fields, indexFile, url) {
